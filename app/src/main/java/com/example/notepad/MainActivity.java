435cba03e7f363bcab0d994fbd40f35f8cbd32db
katherineyecc <katherineyecc@163.com>
2020-01-31 21:29:57 -0500

    Initial commit

---
+++ app/src/main/java/com/example/notepad/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.notepad;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "com.example.notepad.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //TextView editText = (TextView) findViewById(R.id.editText3);
+    }
+
+    public void enterNotepad(View view) {
+        Intent intent = new Intent(this, DisplayNotepad.class);
+        startActivity(intent);
+    }
+}
+
